b670bdb730fc00232d9548aa651ecc9e9ab0b427
Soboroo <woo036325@gmail.com>
2022-01-31 14:59:31 +0900

    access HTML with document object

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const age = parseInt(prompt("How old are you?")); // return type is number!!!
-
-if (isNaN(age)) {
-  alert("You entered incorrect data!");
-} else if (age >= 18) {
-  alert("You are adult!");
-} else {
-  alert("You are not adult!");
-}
+console.log(document);
+const title = document.getElementById("title");
+title.className = "simpleBruh";
