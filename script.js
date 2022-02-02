216f59d8fbdae2edd46fa1097193c6df9e715501
Soboroo <woo036325@gmail.com>
2022-02-02 17:37:55 +0900

    more events

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const title = document.querySelector(".hello h1");
 function handleTitleClick() {
-  title.style.color = "red";
+  title.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is over me!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is leaving me!";
+}
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
