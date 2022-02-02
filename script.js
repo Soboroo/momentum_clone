98daa5c9e2731b9f21ba363dd3946343ae34bdc1
Soboroo <woo036325@gmail.com>
2022-02-02 17:07:17 +0900

    select HTML selector with querySelector(All)

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-console.log(document);
-const title = document.getElementById("title");
-title.innerText = "simpleBruh";
-for (let i = 0; i < document.getElementsByClassName("title").length; i++) {
-  document.getElementsByClassName("title")[i].innerText = "simpleBruh";
-}
+const title = document.querySelectorAll(".hello h1");
+console.log(title);
