9b8f1b87557623f55ed69fa55a3241af7adae66e
Soboroo <woo036325@gmail.com>
2022-02-02 17:21:35 +0900

    event

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const title = document.querySelectorAll(".hello h1");
-console.log(title);
+const title = document.querySelector(".hello h1");
+function handleTitleClick() {
+  title.style.color = "red";
+}
+
+title.addEventListener("click", handleTitleClick);
